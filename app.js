c486d43113cd0c154175715b1251da91a25faa87
arito7 <64824933+arito7@users.noreply.github.com>
2022-03-22 06:44:32 +0900

    Setup mongoose and implement catalog router

--- app.js
+++ app.js
@@ -3,7 +3,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
 
+const dev_db_url =
+  'mongodb+srv://dev:devpass123@inventory-application.rj36g.mongodb.net/devdb?retryWrites=true&w=majority';
+const mongodb = process.env.MONGODB_URI || dev_db_url;
+
+mongoose.connect(mongodb, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error'));
+db.on('connection', () => {
+  console.log('connected to db');
+});
+
+// routers
+const catalogRouter = require('./routes/catalog');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -21,14 +35,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
