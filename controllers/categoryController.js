16d773aab25533e3d5af8ae9fcc0f3c6656deda0
arito7 <64824933+arito7@users.noreply.github.com>
2022-03-22 23:36:37 +0900

    Implement controller skeleton

---
+++ controllers/categoryController.js
@@ -0,0 +1,35 @@
+const Item = require('../models/Item');
+const Category = require('../models/Category');
+const async = require('async');
+const { body, validationResult } = require('express-validator');
+
+exports.create_get = (req, res, next) => {
+  res.send('Not implemented yet');
+};
+
+exports.create_post = (req, res, next) => {
+  res.send('Not implemented yet');
+};
+
+exports.update_get = (req, res, next) => {
+  res.send('Not implemented yet');
+};
+
+exports.update_post = (req, res, next) => {
+  res.send('Not implemented yet');
+};
+
+exports.delete_get = (req, res, next) => {
+  res.send('Not implemented yet');
+};
+
+exports.delete_post = (req, res, next) => {
+  res.send('Not implemented yet');
+};
+
+exports.categories_get = (req, res, next) => {
+};
+
+exports.detail_get = (req, res, next) => {
+  res.send('Not implemented yet');
+};
