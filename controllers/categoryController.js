da492378f7af44ee10d96d70c9701cf3e8507a80
arito7 <64824933+arito7@users.noreply.github.com>
2022-03-23 05:55:51 +0900

    Implement category create feature

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -3,13 +3,35 @@ const Category = require('../models/Category');
 const async = require('async');
 const { body, validationResult } = require('express-validator');
 
+const genreValidationSchema = [
+  body('name', 'Name must not be empty').trim().isLength({ min: 3 }).escape(),
+];
+
 exports.create_get = (req, res, next) => {
-  res.send('Not implemented yet');
+  res.render('./forms/category-form', { title: 'Create Category' });
 };
 
-exports.create_post = (req, res, next) => {
-  res.send('Not implemented yet');
-};
+exports.create_post = [
+  genreValidationSchema,
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.render('./forms/category-form', {
+        title: 'Create Category',
+        errors: errors.array(),
+      });
+    }
+    const category = new Category({
+      name: req.body.name,
+    });
+    category.save((err) => {
+      if (err) {
+        next(err);
+      }
+      res.redirect('/catalog/categories');
+    });
+  },
+];
 
 exports.update_get = (req, res, next) => {
   res.send('Not implemented yet');
