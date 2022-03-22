16b7498e0b17610c629ed1f4b5acc085e92c5289
arito7 <64824933+arito7@users.noreply.github.com>
2022-03-22 23:37:41 +0900

    Implement controllers in catalog route

--- routes/catalog.js
+++ routes/catalog.js
@@ -2,7 +2,43 @@ const express = require('express');
 const router = express.Router();
 
 const itemController = require('../controllers/itemController');
+const categoryController = require('../controllers/categoryController');
+// ITEM ROUTES
 
 router.get('/', itemController.index);
+
+router.get('/item/:id/create', itemController.create_get);
+
+router.post('/item/:id/create', itemController.create_post);
+
+router.get('/item/:id/update', itemController.update_get);
+
+router.post('/item/:id/update', itemController.update_post);
+
+router.get('/item/:id/delete', itemController.delete_get);
+
+router.post('/item/:id/delete', itemController.delete_post);
+
+router.get('/item/:id', itemController.detail_get);
+
+router.get('/items', itemController.items_get);
+
+// CATEGORY ROUTES
+
+router.get('/category/:id/create', categoryController.create_get);
+
+router.post('/category/:id/create', categoryController.create_post);
+
+router.get('/category/:id/update', categoryController.update_get);
+
+router.post('/category/:id/update', categoryController.update_post);
+
+router.get('/category/:id/delete', categoryController.delete_get);
+
+router.post('/category/:id/delete', categoryController.delete_post);
+
+router.get('/category/:id', categoryController.detail_get);
+
+router.get('/categories', categoryController.categories_get);
 
 module.exports = router;
