405f98b2573eb26ffedfe059eb15b70d71de5620
arito7 <64824933+arito7@users.noreply.github.com>
2022-03-22 23:36:54 +0900

    Implement item controller skeleton

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -23,3 +23,35 @@ exports.index = function (req, res) {
     }
   );
 };
+
+exports.create_get = (req, res, next) => {
+  res.send('Not implemented yet');
+};
+
+exports.create_post = (req, res, next) => {
+  res.send('Not implemented yet');
+};
+
+exports.update_get = (req, res, next) => {
+  res.send('Not implemented yet');
+};
+
+exports.update_post = (req, res, next) => {
+  res.send('Not implemented yet');
+};
+
+exports.delete_get = (req, res, next) => {
+  res.send('Not implemented yet');
+};
+
+exports.delete_post = (req, res, next) => {
+  res.send('Not implemented yet');
+};
+
+exports.items_get = (req, res, next) => {
+  res.send('Not implemented yet');
+};
+
+exports.detail_get = (req, res, next) => {
+  res.send('Not implemented yet');
+};
