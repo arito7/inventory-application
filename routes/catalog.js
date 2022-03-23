7a1b5229acc5e4fa80b192445a501e922a1aadd6
arito7 <64824933+arito7@users.noreply.github.com>
2022-03-24 08:36:35 +0900

    Add and setup multer

--- routes/catalog.js
+++ routes/catalog.js
@@ -1,5 +1,40 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const path = require('path');
+
+// set storage engine
+const storage = multer.diskStorage({
+  destination: 'public/uploads/',
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}${file.originalname}`);
+  },
+});
+
+// check file type
+function checkFileType(file, cb) {
+  const filetypes = /jpg|jpeg|png|gif/i;
+
+  // check extension
+  const extname = filetypes.test(path.extname(file.originalname));
+
+  // check MIME Type
+  const mimetype = filetypes.test(file.mimetype);
+
+  if (extname && mimetype) {
+    return cb(null, true);
+  } else {
+    cb('Unsupported file type.');
+  }
+}
+
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 5000000 },
+  fileFilter: (req, file, cb) => {
+    checkFileType(file, cb);
+  },
+});
 
 const itemController = require('../controllers/itemController');
 const categoryController = require('../controllers/categoryController');
@@ -9,11 +44,15 @@ router.get('/', itemController.index);
 
 router.get('/item/create', itemController.create_get);
 
-router.post('/item/create', itemController.create_post);
+router.post('/item/create', upload.single('image'), itemController.create_post);
 
 router.get('/item/:id/update', itemController.update_get);
 
-router.post('/item/:id/update', itemController.update_post);
+router.post(
+  '/item/:id/update',
+  upload.single('image'),
+  itemController.update_post
+);
 
 router.get('/item/:id/delete', itemController.delete_get);
 
