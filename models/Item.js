05048c543edfcace35db20a6d5dc16f0a2c2d206
arito7 <64824933+arito7@users.noreply.github.com>
2022-03-24 08:31:14 +0900

    Add img filename property to Item schema

--- models/Item.js
+++ models/Item.js
@@ -10,10 +10,15 @@ const ItemSchema = new Schema({
   ],
   price: { type: Number, required: true, min: 0 },
   number_in_stock: { type: Number, min: 0, default: 0 },
+  img_filename: { type: String, default: '' },
 });
 
 ItemSchema.virtual('url').get(function () {
   return `/catalog/item/${this._id}`;
+});
+
+ItemSchema.virtual('img_url').get(function () {
+  return `/uploads/${this.img_filename}`;
 });
 
 module.exports = mongoose.model('Item', ItemSchema);
